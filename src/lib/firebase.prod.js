b0e58ad4fddaccc48bffefec1f3b5a511e98edf7
hari0409 <73524123+hari0409@users.noreply.github.com>
2021-08-16 18:27:57 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,12 +5,12 @@ import "firebase/auth";
 
 // Configuration
 const config={
-    apiKey: "AIzaSyB54eCSa1fCIBmvwzQR8LoAtEjojxju5OI",
-    authDomain: "knight-313b8.firebaseapp.com",
-    projectId: "knight-313b8",
-    storageBucket: "knight-313b8.appspot.com",
-    messagingSenderId: "905385884296",
-    appId: "1:905385884296:web:4fefa16876796350343d3d"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 const firebase=Firebase.initializeApp(config);
 // // // // // // seedDatabase(firebase);
